d155bc37bf6a95d0ab27f5fa04d1da61d0dcceaa
Elena Spivak <spivak.e.v@gmail.com>
2020-11-24 18:16:35 +0300

    feat: add the Create page and the Search page

---
+++ src/screens/CreateProduct.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CreateForm from '../components/CreateForm'
+
+const CreateProduct = () => {
+  return (
+    <div className="Create">
+      <h1>Create page</h1>
+      <CreateForm/>
+    </div>
+  );
+}
+
+export default CreateProduct;
---
+++ src/screens/SearchProduct.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SearchForm from '../components/SearchForm'
+
+const SearchProduct = () => {
+  return (
+    <div className="Search">
+      <h1>Search page</h1>
+      <SearchForm />
+    </div>
+  );
+}
+
+export default SearchProduct;
